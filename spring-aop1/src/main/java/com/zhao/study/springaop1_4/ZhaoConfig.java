39fe1926b46786a39da32500d8461648297bf796
RyzeZhao <643080112@qq.com>
2019-12-16 14:47:35 +0800

    Spring：AOP-this和target的小区别：​CGLIB代理方式下，测试测试target会不会打印before

--- spring-aop1/src/main/java/com/zhao/study/springaop1_4/ZhaoConfig.java
+++ spring-aop1/src/main/java/com/zhao/study/springaop1_4/ZhaoConfig.java
@@ -8,6 +8,5 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @ComponentScan("com.zhao.study.springaop1_4")
 ////proxyTargetClass = true更改为CGLIB代理方式
-//@EnableAspectJAutoProxy(proxyTargetClass = true)
-@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ZhaoConfig {}
