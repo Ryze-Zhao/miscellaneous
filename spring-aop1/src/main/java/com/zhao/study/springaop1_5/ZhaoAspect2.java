27182eb9638c3347d0fe65d41848fea72151aaca
RyzeZhao <643080112@qq.com>
2019-12-16 16:11:54 +0800

    Spring：AOP-通知（Advice）各种通知方式演示：针对一连接点（方法），被多切面拦截时（切点）

--- spring-aop1/src/main/java/com/zhao/study/springaop1_5/ZhaoAspect2.java
+++ spring-aop1/src/main/java/com/zhao/study/springaop1_5/ZhaoAspect2.java
@@ -8,35 +8,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-@Order(1)
-public class ZhaoAspect {
+@Order(2)
+public class ZhaoAspect2 {
     @Pointcut(value = "execution(* com.zhao.study.springaop1_5.service..*.*(..))")
     public void pointCut() {}
 
     @Before("pointCut()")
     public void before() {
-        System.out.println("ZhaoAspect-----------------before---------------------------------");
+        System.out.println("ZhaoAspect2-----------------before---------------------------------");
     }
     @After("pointCut()")
     public void after() {
-        System.out.println("ZhaoAspect-----------------after---------------------------------");
+        System.out.println("ZhaoAspect2-----------------after---------------------------------");
     }
     @Around("pointCut()")
     public void around(ProceedingJoinPoint joinPoint) {
-        System.out.println("ZhaoAspect-----------------around---------------------------------start");
+        System.out.println("ZhaoAspect2-----------------around---------------------------------start");
         try {
             joinPoint.proceed();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
-        System.out.println("ZhaoAspect-----------------around---------------------------------end");
+        System.out.println("ZhaoAspect2-----------------around---------------------------------end");
     }
     @AfterReturning("pointCut()")
     public void afterReturning() {
-        System.out.println("ZhaoAspect-----------------afterReturning---------------------------------");
+        System.out.println("ZhaoAspect2-----------------afterReturning---------------------------------");
     }
     @AfterThrowing("pointCut()")
     public void afterThrowing() {
-        System.out.println("afterThrowing---------------------------------");
+        System.out.println("afterThrowing2---------------------------------");
     }
 }
